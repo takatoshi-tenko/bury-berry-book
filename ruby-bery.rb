a1db2c59291294d2341a618c7f738516efaa8a83
takatoshi.tenko <chanten811@gmail.com>
2024-07-20 21:11:11 +0900

    1.9まで

--- ruby-bery.rb
+++ ruby-bery.rb
@@ -16,12 +16,12 @@ fruits = ['apple', 'banana', 'cherry']
 # p fruits.delete_if
 # fruits.each.with_index(1) { |fruit, i| puts "#{i}: #{fruit}" }
 
-dimensions = [
-  [10, 20],
-  [30, 40],
-  [50, 60]
-]
-areas = []
+# dimensions = [
+#   [10, 20],
+#   [30, 40],
+#   [50, 60]
+# ]
+# areas = []
 # dimensions.each do |dimension|
 #   length = dimension[0]
 #   width = dimension[1]
@@ -34,18 +34,59 @@ areas = []
 # end
 # pp areas
 
-dimensions.each_with_index do |(length, width), i|
-  puts "length: #{length}, width: #{width}, i: #{i}"
-end
+# dimensions.each_with_index do |(length, width), i|
+#   puts "length: #{length}, width: #{width}, i: #{i}"
+# end
+
+# pp ['1', '20', '300'].map { |s| s.rjust(3, '0') }
+
+# 番号指定パラメータを使わない場合（ブロックパラメータが2つ）
+# pp ['japan', 'us', 'italy'].map.with_index { |country, n| [n, country] }
+#=> [[0, "japan"], [1, "us"], [2, "italy"]]
+
+# 番号指定パラメータを使う場合
+# pp ['japan', 'us', 'italy'].map.with_index { [_2, _1] }
+#=> [[0, "japan"], [1, "us"], [2, "italy"]]
 
 
+# dimensions = [
+#   [10, 20],
+#   [30, 40],
+#   [50, 60]
+# ]
 
+# dimensions.each do |p_1|
+#   p p_1
+# end
 
+# dimensions.each { puts "#{_1} / #{_2}" }
 
+# sum = 0
+# [[1,2,3], [4,5,6]].each do |value|
+#   value.each do
+#     sum += _1
+#   end
+# end
+# p sum
 
+# pp ['1', '2', '3'].map { |s| _1.rjust(3, '0') }
+# File.open('./sample.txt', 'w') do |file|
+#   file.puts '1行目のテキストです。'
+#   file.puts '2行目のテキストです。'
+#   file.puts '3行目のテキストです。'
+# end
 
+# a = [1,2,3]
+# pp a.delete 100 do
+#   'NG'
+# end
+# p a
 
+# pp a.delete(100) { 'NG' }
 
+name = ["佐藤", "鈴木", "田中"]
+san_name =  name.map { |name| "#{name}さん" }.join('と')
+pp san_name
 
 
 
