7480c3395d1f6c53745b23aab8116040f78634b3
takatoshi.tenko <chanten811@gmail.com>
2024-07-20 14:28:00 +0900

    with_indexの類似処理

--- ruby-bery.rb
+++ ruby-bery.rb
@@ -1,12 +1,13 @@
 puts 'Hello world!'
-
 a = 1
 b = 2
 c = a + b
 puts c
 
 # 2024/07/19
-
 fruits = ['apple', 'banana', 'cherry']
-# fruits.each_with_index { |fruit, i| puts "#{i}: #{fruit}" }
-fruits.map.with_index { |fruit, i| "#{i}: #{fruit}" }
+pp 'each_with_index', fruits.each_with_index { |fruit, i| puts "#{i}: #{fruit}" }
+pp 'map.with_index', fruits.map.with_index { |fruit, i| "#{i}: #{fruit}" }
+
+# 2024/07/20
+pp 'delete_if.with_index', fruits.delete_if.with_index { |fruit, i| fruit.include?('a') && i.odd? }
