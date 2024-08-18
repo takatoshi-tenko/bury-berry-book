c400d225f5f16bbef2f8e7e93824912aa214fa89
takatoshi.tenko <chanten811@gmail.com>
2024-08-18 21:52:23 +0900

    refactor and add: リファクタとテスト項目追加

--- ruby-book/test/gate_test.rb
+++ ruby-book/test/gate_test.rb
@@ -3,28 +3,38 @@ require_relative '../lib/gate'
 require_relative '../lib/ticket'
 
 class GateTest < Minitest::Test
-  def test_gate
-    assert Gate.new
-  end
   # テストシナリオ
   # 160円の切符を購入する。
   # 梅田で入場し、十三で出場する。
   # 期待する結果: 出場できる。
-  def test_gate
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
 
-    ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
+
+  def test_umeda_to_juso
+    ticket = Ticket.new(160)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
   end
 
   def test_umeda_to_mikuni_when_fare_is_not_enough
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
+    ticket = Ticket.new(160)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
+  end
 
-    ticket = Ticket.new(150)
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket)
+  def test_umeda_to_mikuni_when_fare_is_enough
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
+
+  def test_juso_to_mikuni
+    ticket = Ticket.new(160)
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
   end
 end
