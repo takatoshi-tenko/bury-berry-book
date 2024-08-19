49a3de625ffe95a0f896a8e4d9fd37d006cca845
takatoshi.tenko <chanten811@gmail.com>
2024-08-20 00:03:57 +0900

    パターンマッチの実装

--- ruby-bery.rb
+++ ruby-bery.rb
@@ -764,3 +764,41 @@
 # greet do |text|
 #   text * 2
 # end
+
+# records = [
+#   [2021],
+#   [2019, 5],
+#   [2018, 4, 1],
+# ]
+
+# require 'date'
+
+# records.each do |record|
+#   case record.size
+#   when 1
+#     puts Date.new(record[0], 1, 1)
+#   when 2
+#     puts Date.new(record[0], record[1], 1)
+#   when 3
+#     puts Date.new(record[0], record[1], record[2])
+#   end
+# end
+
+# records.each do |record|
+#   case record
+#   in [y]
+#     puts Date.new(y, 1, 1)
+#   in [y, m]
+#     puts Date.new(y, m, 1)
+#   in [y, m, d]
+#     puts Date.new(y, m, d)
+#   end
+# end
+
+
+
+
+
+
+
+
