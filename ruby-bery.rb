09c20b1f3ec96b95f4e6995f1edf1c7990e0648a
takatoshi.tenko <chanten811@gmail.com>
2024-08-19 23:21:27 +0900

    例外処理の実装

--- ruby-bery.rb
+++ ruby-bery.rb
@@ -640,3 +640,55 @@
 
 # pp Baseball::Second.new('Alice', 13)
 # pp Clock::Second.new(13)
+
+# puts 'Start.'
+
+# begin 
+#   1 + '10'
+# rescue
+#   puts '例外が発生しました'
+# end
+
+# puts 'End.'
+
+# def method_1
+#   puts 'method_1 start.'
+#   begin
+#     method_2
+#   rescue
+#     puts '例外が発生しました'
+#   end
+#   puts 'method_1 end.'
+# end
+
+# def method_2
+#   puts 'method_2 start.'
+#   method_3
+#   puts 'method_2 end.'
+# end
+
+# def method_3
+#   puts 'method_3 start.'
+#   1 / 0
+#   puts 'method_3 end.'
+# end
+
+# method_1
+
+# begin 
+#   1 / 0
+# rescue => e
+#   puts "エラークラス: #{e.class}"
+#   puts "エラーメッセージ: #{e.message}"
+#   puts "バックトレース -----"
+#   puts e.backtrace
+#   puts "-----"
+# end
+
+# begin
+#   'abc'.foo
+# rescue NameError
+#   puts 'NameErrorです'
+# rescue NoMethodError
+#   puts 'NoMethodErrorです'
+# end
