891dfff63e3c8e3bcbed30d1d6fbbf8b33bc82f0
takatoshi.tenko <chanten811@gmail.com>
2024-08-27 00:12:04 +0900

    fix: 初級編20~34問の回答

--- practiceHundred/main.rb
+++ practiceHundred/main.rb
@@ -154,6 +154,164 @@
 
 # 初級編
 # --------------------------------
+# No. 20 割って掛ける
+# 整数値を2つ入力させ、1つめの値を2つめの値で割った結果を表示し、続けてその結果に2つめの値を掛けた結果を表示するプログラムを作成せよ。計算はすべて整数型で行うこと（割り切れない場合は自動的に小数点以下が切り捨てられる）。
+# print "input 1st value: "
+# n1 = gets.to_i
+# print "input 2nd value: "
+# n2 = gets.to_i
+# result1 = n1 / n2
+# puts "result: #{result1}"
+# puts "result: #{result1 * n2}"
+# --------------------------------
+# No. 21 5より大きく20より小さい
+# 整数値を入力させ、その値が5よりも大きく、かつ、20よりも小さければOKと表示するプログラムを作成せよ。
+# print "input value: "
+# n = gets.to_i
+# puts "OK" if n > 5 && n < 20
+# --------------------------------
+# No. 22 -10以下または10以上
+# 整数値を入力させ、その値が-10以下、または、10以上であればOKと表示するプログラムを作成せよ。
+# print "input value: "
+# n = gets.to_i
+# puts "OK" if n <= -10 || n >= 10
+# --------------------------------
+# No. 23 -5以上10未満
+# 整数値を入力させ、その値が-5以上10未満であればOK、そうでなければNGと表示するプログラムを作成せよ。
+# print "input value: "
+# n = gets.to_i
+# if -5 <= n && n < 10
+#   puts "OK"
+# else
+#   puts "NG"
+# end
+# --------------------------------
+# No. 24 -10以上0未満、または、10以上
+# 整数値を入力させ、その値が-10以上0未満、または、10以上であればOK、そうでなければNGと表示するプログラムを作成せよ。
+# print "input value: "
+# n = gets.to_i
+# if -10 <= n && n < 0 || 10 <= n
+#   puts "OK"
+# else
+#   puts "NG"
+# end
+# --------------------------------
+# No. 25 -10未満?、-10以上0未満?、0以上?
+# 整数値を入力させ、その値が-10未満ならrange 1、-10以上0未満であればrange 2、0以上であればrange 3、と表示するプログラムを作成せよ。
+# print "input value: "
+# n = gets.to_i
+# if n < -10
+#   puts "range 1"
+# elsif -10 <= n && n < 0
+#   puts "range 2"
+# else
+#   puts "range 3"
+# end
+# --------------------------------
+# No. 26 switch-case
+# 整数値を入力させ、その値が1ならone、2ならtwo、3ならthree、それ以外ならothersと表示するプログラムをswicth-case文を使って作成せよ。
+# print "input value: "
+# n = gets.to_i
+# case n
+# when 1
+#   puts "one"
+# when 2
+#   puts "two"
+# when 3
+#   puts "three"
+# else
+#   puts "others"
+# end
+# --------------------------------
+# No. 27 1からnまでの和
+# 整数値を入力させ、1からその値までの総和を計算して表示するプログラムを作成せよ。ただし、0以下の値を入力した場合は0と表示する。
+# print "input value: "
+# n = gets.to_i
+# sum = 0
+# n.times do |i|
+#   sum += i
+# end 
+# if n < 0
+#   puts 0
+# else
+#   puts sum + n
+# end
+# --------------------------------
+# No. 28 nの階乗
+# 整数値を入力させ、その値の階乗を表示するプログラムを作成せよ。ただし、0以下の値を入力した場合は1と表示する。
+# print "input value: "
+# n = gets.to_i
+# result = 1
+# if n <= 0
+#   puts 1
+# else
+#   n.times do |i|
+#     result *= i + 1
+#   end
+#   puts result
+# end
+# --------------------------------
+# No. 29 5つの合計
+# 整数値を5回入力させ、それらの値の合計を表示するプログラムを繰り返しを使って作成せよ。
+# print "input value: "
+# n1 = gets.to_i
+# print "input value: "
+# n2 = gets.to_i
+# print "input value: "
+# n3 = gets.to_i
+# print "input value: "
+# n4 = gets.to_i
+# print "input value: "
+# n5 = gets.to_i
+# puts "sum: #{n1 + n2 + n3 + n4 + n5}"
+# --------------------------------
+# No. 30 棒グラフ
+# 整数値を入力させ、その個数だけ*を表示するプログラムを作成せよ。入力値が0以下の値の場合は何も書かなくてよい。
+# print "input value: "
+# n = gets.to_i
+# result = []
+# n.times { result << '*'}
+# puts result.join('')
+# --------------------------------
+# No. 31 棒グラフ改
+# 整数値を入力させ、その個数だけ*を、5個おきに空白（スペース）を入れて表示するプログラムを作成せよ。入力値が0以下の値の場合は何も書かなくてよい。
+# print "input value: "
+# n = gets.to_i
+# result = []
+# sum = 1
+# n.times do |i|
+#   result << '*'
+#   sum = i + 1
+#   result << ' ' if sum % 5 == 0
+# end
+# puts result.join('')
+# --------------------------------
+# No. 32 5の倍数でbar
+# 1から20まで順に表示するが、5の倍数の場合は数字の代わりにbarと表示するプログラムを作成せよ。
+# 1.upto(20) do |i|
+#   if i % 5 == 0
+#     puts "bar"
+#   else
+#     puts i
+#   end
+# end
+# --------------------------------
+# No. 33 入力値抜き
+# 整数値を入力させ、1から9まで、入力値以外を表示するプログラムを作成せよ。
+# print "input value: "
+# n = gets.to_i
+# 1.upto(9) do |i|
+#   puts i if n != i
+# end
+# --------------------------------
+# No. 34 入力値抜き改
+# 整数値を入力させ、1から9まで、入力値と入力値+1以外を表示するプログラムを作成せよ。入力値が9の場合は9のみ表示しない。
+# print "input value: "
+# n = gets.to_i
+# 1.upto(9) do |i|
+#   puts i if n != i && n+1 != i
+# end
+# --------------------------------
 # No. 35 配列要素の参照
 # {3, 7, 0, 8, 4, 1, 9, 6, 5, 2}で初期化される大きさ10の整数型配列を宣言し、整数値を入力させ、
 # 要素番号が入力値である配列要素の値を表示するプログラムを作成せよ。入力値が配列の要素の範囲外であるかどうかのチェックは省略してよい。
